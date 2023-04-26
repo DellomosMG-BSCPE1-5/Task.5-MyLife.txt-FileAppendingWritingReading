79db58ec01caf513a1c5545ae78176d981884b5b
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 08:03:24 +0700

    PSEUDOCODE

---
+++ main_code.py
@@ -0,0 +1,8 @@
+#Write a method in python to write multiple line of text contents
+#into a text file mylife.txt
+
+#Define a function 
+#Open a text file named mylife.txt (append)
+#Ask user for a line text input
+#Append/write the input text to mylife.txt file
+#Ask the user if he/she wants to enter a line of text again
