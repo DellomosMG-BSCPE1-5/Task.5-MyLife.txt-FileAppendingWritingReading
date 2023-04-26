9b8067bb2371cd72aab18e7c98bea8df0b57f19f
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 12:25:36 +0700

    Commands for the buttons to work

--- GUI-involved-program.py
+++ GUI-involved-program.py
@@ -1,5 +1,24 @@
 #CREATION OF GUI
 from tkinter import *
+
+#1st COMMAND: Get the entries inputted by the user
+def save_entries():
+    date_of_entry = entry_date_input.get()
+    entry = entry_text_input.get()
+#Open a text file and name it "mylife.txt"
+    with open("mylife.txt", "a") as main_file:
+#Append/write the input text to mylife.txt file
+        main_file.write(str(date_of_entry) + "\n" + str(entry) + "\n\n")
+
+#2nd COMMAND: Deletes the previous entry of the user from the text box in order to enter new line of texts
+def try_again():
+      entry_date_input.delete(0, "end")
+      entry_text_input.delete(0, "end")
+
+#3rd COMMAND: Quits and Exits the GUI Window
+def close():
+        file.destroy()
+
 #Create the main window that will be appear on the screen 
 file = Tk()
 file.geometry("325x300")
@@ -16,13 +35,15 @@ entry_date_text.place(x=10, y=55)
 entry_text_label = Label(text="How was your day?", fg="teal", bg="#F1F6F5", font=("courier new", 14, "bold"))
 entry_text_label.place(x=10, y=105)
 
-#Create text box where the user will input their entry
+#Create text box where the user will input their entry; and
+#Ask user to input their entry.
 entry_date_input = Entry(width="48")
 entry_date_input.place(x=10,y=80, width=301, height=25)
 entry_text_input = Entry(width="48")
 entry_text_input.place(x=10,y=130, width=301, height=25)
 
 #Create necessary buttons
+#Link commands to the buttons
 button = Button(file,text="Submit", command=save_entries, width="32", height="1", bg="#71ADB5", fg="white", font=("courier new", 11, "bold"))
 button.place(x=11, y=185)
 
@@ -32,12 +53,6 @@ button.place(x=11, y=220)
 button = Button(file,text="Quit", command=close, width="32", height="1", bg="#0D3446", fg="white", font=("courier new", 11, "bold"))
 button.place(x=11, y=255)
 
+#CREATION OF COMMANDS FOR THE BUTTONS TO FOLLOW (LOCATED AT THE TOP OF THE PROGRAM)
+ 
 mainloop()
-#CREATION OF COMMANDS FOR THE BUTTONS TO FOLLOW
-#Ask user to input their entry in the 
-#1st COMMAND: Get the entries inputted by the user
-#Open a text file and name it "mylife.txt"
-#Append/write the input text to mylife.txt file
-#Link commands to the buttons
-#2nd COMMAND: Deletes the previous entry of the user from the text box in order to enter new line of texts
-#3rd COMMAND: Quits and Exits the GUI Window
