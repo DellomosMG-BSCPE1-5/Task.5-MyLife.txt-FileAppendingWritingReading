af76fed5ef24c35dbccd7ec99d4fdc30fbabeea6
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 20:02:46 +0700

    The main code has been decorated using pyfiglet

--- main_code.py
+++ main_code.py
@@ -1,20 +1,28 @@
 #Write a method in python to write multiple line of text contents
 #into a text file mylife.txt
 
-#Define a function 
-def entry_writing():
-    #Open a text file named mylife.txt (append)
-    while True:
+while True:
+    #HEADER 
+    import pyfiglet
+    from colorama import Fore, Back, Style
+    print(Fore.BLUE + pyfiglet.figlet_format("  My Life   ", font = "xsansb", width=500))
+
+    #Define a function
+    def entry_writing():
+        #Open a text file named mylife.txt (append)
         with open("mylife.txt", "a") as main_file:
             #Ask user for a line text input
-            date_of_entry = input("Date of Entry: ")
-            entry = input(str("Kindly enter a text: "))
+            print(Fore.BLUE + Style.BRIGHT + "✨ Date: " + Fore.LIGHTCYAN_EX + Style.NORMAL,  end = "")
+            date_of_entry = input(Fore.WHITE + "")
+            print(Fore.BLUE + Style.BRIGHT + "\n✨ Your Entry: " + Fore.LIGHTCYAN_EX + Style.NORMAL,  end = "")
+            entry = input(Fore.WHITE + "")
             #Append/write the input text to mylife.txt file
             main_file.write((str(date_of_entry) + "\n" + str(entry) + "\n\n"))            
-            #Ask the user if he/she wants to enter a line of text again
-            check = input("\nDo you want to start again? Yes or No: ")
-            #Go back to top
-            if check.upper() == "Y" or check == "yes" or check == "YES" or check == "Yes":  
-                continue
-        break
-entry_writing()   
+
+    entry_writing() 
+
+    #Ask the user if he/she wants to enter text again
+    check = input(Fore.YELLOW + "\nDo you want to add an entry again? Yes or No? ") 
+    if check.upper() == "Y" or check == "yes" or check == "YES" or check == "Yes":  
+        continue
+    break
