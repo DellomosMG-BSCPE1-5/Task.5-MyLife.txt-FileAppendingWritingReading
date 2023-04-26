e8de3874c475757e07b7a944c79e7d7e1abc179b
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 08:31:53 +0700

    Append/write the user inputs to mylife.txt file

--- main_code.py
+++ main_code.py
@@ -4,11 +4,12 @@
 #Define a function 
 def entry_writing():
     #Open a text file named mylife.txt (append)
-    with open("mylife.txt") as main_file:
+    with open("mylife.txt", "a") as main_file:
         #Ask user for a line text input
         date_of_entry = input("Date of Entry: ")
         entry = input(str("Kindly enter a text: "))
     #Append/write the input text to mylife.txt file
+        main_file.write((str(date_of_entry) + "\n" + str(entry) + "\n\n"))
     #Ask the user if he/she wants to enter a line of text again
 
 entry_writing()
