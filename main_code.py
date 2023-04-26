681ec6987b268f39741f18080323b7b2949c50ee
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 08:22:26 +0700

    Asking user for input

--- main_code.py
+++ main_code.py
@@ -5,9 +5,10 @@
 def entry_writing():
     #Open a text file named mylife.txt (append)
     with open("mylife.txt") as main_file:
-        for line in main_file:
-            print(line)
+        #Ask user for a line text input
+        date_of_entry = input("Date of Entry: ")
+        entry = input(str("Kindly enter a text: "))
+    #Append/write the input text to mylife.txt file
+    #Ask the user if he/she wants to enter a line of text again
+
 entry_writing()
-#Ask user for a line text input
-#Append/write the input text to mylife.txt file
-#Ask the user if he/she wants to enter a line of text again
