6bfcaf7399ad520d584f442eb643148f90753f29
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 09:34:12 +0700

    Pseudocode

---
+++ GUI-involved-program.py
@@ -0,0 +1,14 @@
+#CREATION OF GUI
+#Create the main window that will be appear on the screen 
+#Create the header and labels that instructs the user what to enter
+#Create text box where the user will input their entry
+#Create necessary buttons
+
+#CREATION OF COMMANDS FOR THE BUTTONS TO FOLLOW
+#Ask user to input their entry in the 
+#1st COMMAND: Get the entries inputted by the user
+#Open a text file and name it "mylife.txt"
+#Append/write the input text to mylife.txt file
+#Link commands to the buttons
+#2nd COMMAND: Deletes the previous entry of the user from the text box in order to enter new line of texts
+#3rd COMMAND: Quits and Exits the GUI Window
